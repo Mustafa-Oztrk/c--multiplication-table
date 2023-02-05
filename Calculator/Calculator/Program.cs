2b07738c495b76a65f17b8fcbe233e59c4060b00
By Red Code <68156621+By-Red-Code@users.noreply.github.com>
2023-02-05 21:13:41 +0300

    C# Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 1; i <= 10; i++)
+{
+
+	Console.WriteLine("-----------");
+	for (int j = 1; j <= 10 ; j++)
+	{
+		Console.WriteLine(i + "X" + j + "=" + (i*j));
+	}
+}
